b148f422e442e5bae8cfcbe1e117099e063c56d5
Will Tuffrey <william.tuffrey@yahoo.com>
2026-01-28 16:18:12 +0000

    Start on custom dropdown menus

--- Code/scripts.js
+++ Code/scripts.js
@@ -1,3 +1,3 @@
 function Function1(x) {    // Test function
-    document.getElementById("demo").innerHTML = "Paragraph changed." + x;
+    document.getElementById("demo").innerHTML = "Paragraph changed " + x;
 }
