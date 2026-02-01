8a5bfd90ca76637456a76b43012f8361dcbca010
Will Tuffrey <william.tuffrey@yahoo.com>
2026-02-01 21:31:17 +0000

    Redesign of the footer and main page content

--- Code/scripts.js
+++ Code/scripts.js
@@ -1,6 +1,13 @@
 function Function1(x) {    // Test function
-    document.getElementById("demo").innerHTML = "Paragraph changed " + x;
+    if (x == 1) {
+        document.getElementById("demo").innerHTML = "Paragraph changed to paragraph one.";
+    }
+    else if (x == 2) {
+        document.getElementById("demo").innerHTML = "Paragraph changed to paragraph two.";
+    }
 }
 function toggleDropdown() {
     // Toggle the dropdown menu visibility and the button image    
+    var dropdownMenu = document.getElementById("button");
+    dropdownMenu.toggleAttribute("active");    
 }
